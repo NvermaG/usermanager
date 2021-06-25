a36ab2e13e7e4153638c85d6b77822b08131a442
Nikhil <uic.18mca1003@gmail.com>
2021-06-25 18:07:05 +0530

    Email Registered

--- usermanagement/settings.py
+++ usermanagement/settings.py
@@ -73,7 +73,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "nikhil.verma@oodles.io"
+EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_DEFAULT_PASSWORD = "abc@123"
 
